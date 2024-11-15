da5469ee98d3c102826dc6d7a7cc9fadee0a7b56
SpiritedVibes <austinrassman@gmail.com>
2024-11-15 14:13:19 -0500

    review.js

--- models/review.js
+++ models/review.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+\
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
@@ -12,3 +12,19 @@ const commentSchema = new mongoose.Schema({
   ],
   createdAt: { type: Date, default: Date.now }
 });
+
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    title: { type: String, required: true }, 
+    body: { type: String, required: true }, 
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+    comments: [commentSchema], 
+    createdAt: { type: Date, default: Date.now }, 
+    updatedAt: { type: Date, default: Date.now } 
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Review', reviewSchema);
