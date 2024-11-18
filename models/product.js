acdd44d4db441eff04cfacd8b8b9b905e915df68
SpiritedVibes <austinrassman@gmail.com>
2024-11-18 09:56:47 -0500

    user, review

--- models/product.js
+++ models/product.js
@@ -1,16 +1,30 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+  replies: [
+    {
+      text: { type: String, required: true },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      createdAt: { type: Date, default: Date.now }
+    }
+  ],
+  createdAt: { type: Date, default: Date.now }
+});
 
 
-const productSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    imgURL: {type: String, required: true},
-    seller: { type: mongoose.Schema.Types.ObjectId, required: true }
-    
-},
-{ timestamps: true }
-)
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    imgURL: { type: String, required: true },
+    seller: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' }, 
+    comments: [commentSchema]
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('product', productSchema)
+module.exports = mongoose.model('Product', productSchema);
