d08705acd91adf37a748b3854a20bd431e8a65b8
SpiritedVibes <austinrassman@gmail.com>
2024-11-18 14:28:11 -0500

    review

---
+++ models/review.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const reviewSchema = new mongoose.Schema({
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+})
+
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = { Review }
--- models/user.js
+++ models/user.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
 
-
-const reviewSchema = new mongoose.Schema({
-  rating: { type: Number, required: true, min: 1, max: 5 },
-  title: { type: String, required: true },
-  body: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
-})
-
-
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -33,7 +22,4 @@ userSchema.set('toJSON', {
 const User = mongoose.model('User', userSchema)
 
 
-const Review = mongoose.model('Review', reviewSchema)
-
-
-module.exports = { User, Review }
+module.exports = { User}
