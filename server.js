7ba712d60e8f51650569257b09268495beb4fa23
Michael Tibbetts <michael.tibbetts.1@gmail.com>
2024-11-19 11:53:43 -0500

    updated signin route

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const profilesRouter = require('./controllers/profiles');
 const productsRouter = require('./controllers/products');
 const usersRouter = require('./controllers/users');
-const authRoutes = require('./controllers/auth');
+// const authRoutes = require('./controllers/auth');
 
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,8 +27,8 @@ app.use(express.json());
 app.use('/users', usersRouter); 
 app.use('/profiles', profilesRouter); 
 app.use('/products', productsRouter); 
-app.use('/auth', authRoutes); 
-console.log(authRoutes)
+// app.use('/auth', authRoutes); 
+// console.log(authRoutes)
 
 
 app.listen(3002, () => {
